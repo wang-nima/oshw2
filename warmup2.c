269f62bd636e71a36ab03ef11d55511c10ecbb0b
shawn <jak47042007@163.com>
2015-02-16 17:17:20 -0800

    pat tpt complete

--- warmup2.c
+++ warmup2.c
@@ -16,18 +16,20 @@ int num = 20;		//total number of packets to arrive
 int n = 4;
 
 pthread_mutex_t mutex;
+pthread_cond_t emptyQ2;
 
 My402List q1, q2;
 
 int tokenBucket;
 
-pthread_t tdt;
+pthread_t tdt, pat;
+
 
 
 typedef struct packet {
 	int packetId;
 	int serviceTime;
-	int tokenNeeded;
+	int tokenRequired;
 	int arrivalTime;
 	int departureTime;
 	int responseTime;
@@ -43,8 +45,42 @@ void setParameter(int argc, char **argv) {
 	} 
 }
 
+packet* createPacket(int id) {
+	packet *ret = (packet*)malloc(sizeof(packet));
+	ret->packetId = id;
+	ret->serviceTime = 1000000/mu;
+	ret->tokenRequired = P;
+	return ret;
+}
+
+void move() {
+	My402ListElem *firstElem = My402ListFirst(&q1);
+	packet *firstPacket = (packet*)firstElem->obj;
+	if(firstPacket->tokenRequired <= tokenBucket) {
+		tokenBucket -= firstPacket->tokenRequired;
+		My402ListAppend(&q2, firstPacket);
+		My402ListUnlink(&q1, firstElem);
+		printf("p%d leaves Q1, time in Q1 = ms, token bucket now has %d token\n", firstPacket->packetId, tokenBucket);
+		if(My402ListEmpty(&q2)) {
+			pthread_cond_signal(&emptyQ2);
+		}
+	}
+}
+
+
 void* packetArrival(void *arg) {
-	
+	packet *p;
+	int i = 0;
+	while(1) {
+		pthread_mutex_lock(&mutex);
+		usleep(1000000/lambda);
+		i++;
+		p = createPacket(i);
+		printf("p%d arrives, needs %d tokens\n", p->packetId, p->tokenRequired);
+		My402ListAppend(&q1,p);
+		printf("p%d enters Q1\n", i);
+		pthread_mutex_unlock(&mutex);
+	}
 	return (void*)0;
 }
 
@@ -52,11 +88,13 @@ void *tokenDeposit(void *arg) {
 	int i = 0;
 	while(1) {
 		pthread_mutex_lock(&mutex);
+		usleep(1000000/r);
 		tokenBucket++;
 		i++;
 		tokenBucket = min(tokenBucket, B);
-		//usleep(10000);			//sleep for 1s
-		usleep(1000000/r);
+		if(!My402ListEmpty(&q1)) {
+			move();
+		}
 		pthread_mutex_unlock(&mutex);
 		printf("token t%d arrives, token bucket now has %d token\n", i, tokenBucket);
 	}
@@ -77,11 +115,16 @@ void init() {
 	tokenBucket = 0;
 	//mutex
 	pthread_mutex_init(&mutex, NULL);
-
+	//condition varibale
+	pthread_cond_init(&emptyQ2, NULL);
+	//q1 and q2
+	My402ListInit(&q1);
+	My402ListInit(&q2);
 }
 
 void createThread() {
 	pthread_create(&tdt, NULL, tokenDeposit, (void*)0);
+	pthread_create(&pat, NULL, packetArrival, (void*)0);
 }
 
 int main(int argc, char **argv) {

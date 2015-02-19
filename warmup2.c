8c933eb1bb11de4414cc90ac11ac1eb7feafbc8b
shawn <jak47042007@163.com>
2015-02-18 19:31:14 -0800

    read from trace file

--- warmup2.c
+++ warmup2.c
@@ -24,11 +24,13 @@ My402List q1, q2;
 
 int tokenBucket;
 
-pthread_t tdt, pat, s1, s2;
+pthread_t tdt, pat, s1, s2, pat_td;
 
 struct timeval start;
 
 FILE *fp;
+
+char filePath[20];
 
 int traceDrivenMode = 0;
 
@@ -46,6 +48,7 @@ typedef struct packet {
 
 	unsigned int interArrivalTime;		//in micro second
 }packet;
+
 
 void setParameter(int argc, char **argv) {
 	int i;
@@ -65,7 +68,8 @@ void setParameter(int argc, char **argv) {
 				num = atoi(argv[i+1]);
 			} else if(strcmp(argv[i] + 1, "t") == 0) {
 				//read from file
-				fp = fopen(argv[i] + 1,"r");
+				fp = fopen(argv[i+1],"r");
+				strcpy(filePath, argv[i+1]);
 				traceDrivenMode = 1;
 			} else {
 				printf("invalid option\n");
@@ -81,7 +85,8 @@ unsigned long currentTimeToMicroSecond() {
 	unsigned long ret;
 	struct timeval t;
 	gettimeofday(&t, NULL);
-	ret = (t.tv_sec * 1000000 + t.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	ret = (t.tv_sec * 1000000 + t.tv_usec) - 
+		  (start.tv_sec * 1000000 + start.tv_usec);
 	return ret;
 }
 
@@ -104,6 +109,15 @@ packet* createPacket(int id) {
 	return ret;
 }
 
+packet* createPacketByTraceFile(int id) {
+	packet *ret = (packet*)malloc(sizeof(packet));
+	ret->packetId = id;
+	fscanf(fp, "%d", &(ret->interArrivalTime));
+	fscanf(fp, "%d", &(ret->tokenRequired));
+	fscanf(fp, "%d", &(ret->serviceTime));
+	return ret;
+}
+
 void move() {
 	My402ListElem *firstElem = My402ListFirst(&q1);
 	packet *firstPacket = (packet*)firstElem->obj;
@@ -116,7 +130,8 @@ void move() {
 		int interval = firstPacket->arrivalQ2 - firstPacket->arrivalQ1;
 		int x = interval / 1000;
 		int y = interval % 1000;
-		printf("p%d leaves Q1, time in Q1 = %d.%dms, token bucket now has %d token\n", firstPacket->packetId, x, y, tokenBucket);
+		printf("p%d leaves Q1, time in Q1 = %d.%dms, token bucket now has %d token\n",
+				firstPacket->packetId, x, y, tokenBucket);
 		printTime();
 		printf("p%d enters Q2\n", firstPacket->packetId);
 		if(!My402ListEmpty(&q2)) {
@@ -137,21 +152,55 @@ void* packetArrival(void *arg) {
 	packet *p;
 	int i = 0;
 	while(1) {
+		i++;
 		sleepWithinTenSecond(1000000/lambda);
 		pthread_mutex_lock(&mutex);
+		p = createPacket(i);
 		if(i == num) {
 			pthread_mutex_unlock(&mutex);
 			return (void*)0;
 		}
-		i++;
-		p = createPacket(i);
 		printTime();
-		printf("p%d arrives, needs %d tokens, inter-arrival time = %d.%dms\n", p->packetId, p->tokenRequired, p->interArrivalTime/1000, p->interArrivalTime%1000);
+		printf("p%d arrives, needs %d tokens, inter-arrival time = %d.%dms\n",
+				p->packetId, p->tokenRequired, p->interArrivalTime/1000, p->interArrivalTime%1000);
 		My402ListAppend(&q1,p);
 		printTime();
 		printf("p%d enters Q1\n", i);
 		pthread_mutex_unlock(&mutex);
 	}
+}
+
+void* packetArrivalTraceDriven(void *arg) {
+	packet *p;
+	int i = 0;
+	while(1) {
+		if(i == num) {
+			return (void*)0;
+		}
+		i++;
+		p = createPacketByTraceFile(i);
+		usleep(p->interArrivalTime * 1000);
+		p->arrivalQ1 = currentTimeToMicroSecond();
+		p->interArrivalTime = p->arrivalQ1 - lastPacketArrivalTimeInMicroSecond;
+		lastPacketArrivalTimeInMicroSecond = p->arrivalQ1;
+		if(p->tokenRequired > B) {
+			printTime();
+			printf("p%d arrives, needs %d tokens, inter-arrival time = %d.%dms, dropped\n",
+					i, p->tokenRequired, p->interArrivalTime / 1000, p->interArrivalTime % 1000);
+			continue;
+		}
+
+		pthread_mutex_lock(&mutex);
+		printTime();
+		printf("p%d arrives, needs %d tokens, inter-arrival time = %d.%dms\n",
+				p->packetId, p->tokenRequired, p->interArrivalTime/1000, p->interArrivalTime%1000);
+		My402ListAppend(&q1,p);
+		printTime();
+		printf("p%d enters Q1\n", i);
+
+		pthread_mutex_unlock(&mutex);
+	}
+	return (void*)0;
 }
 
 void *tokenDeposit(void *arg) {
@@ -198,7 +247,8 @@ void *server1(void *arg) {
 		}
 		p = deleteFirstFromQ2();
 		printTime();
-		printf("p%d begins service at S1, requesting %d.%dms of service\n", p->packetId, p->serviceTime / 1000, p->serviceTime % 1000);
+		printf("p%d begins service at S1, requesting %d.%dms of service\n",
+				p->packetId, p->serviceTime / 1000, p->serviceTime % 1000);
 		pthread_mutex_unlock(&mutex);
 		sleepWithinTenSecond(1000000 / mu);
 		pthread_mutex_lock(&mutex);
@@ -210,7 +260,8 @@ void *server1(void *arg) {
 		int intervalTotal = p->departureTime - p->arrivalQ1;
 		int a = intervalTotal / 1000;
 		int b = intervalTotal % 1000;
-		printf("p%d departs from S1, service time = %d.%dms, time in system = %d.%dms\n", p->packetId, x, y, a ,b);
+		printf("p%d departs from S1, service time = %d.%dms, time in system = %d.%dms\n",
+				p->packetId, x, y, a ,b);
 		pthread_mutex_unlock(&mutex);
 	}
 	return (void*)0;
@@ -227,7 +278,8 @@ void *server2(void *arg) {
 		}
 		p = deleteFirstFromQ2();
 		printTime();
-		printf("p%d begins service at S2, requesting %d.%dms of service\n", p->packetId, p->serviceTime / 1000, p->serviceTime % 1000);
+		printf("p%d begins service at S2, requesting %d.%dms of service\n",
+				p->packetId, p->serviceTime / 1000, p->serviceTime % 1000);
 		pthread_mutex_unlock(&mutex);
 		sleepWithinTenSecond(1000000 / mu);
 		pthread_mutex_lock(&mutex);
@@ -239,7 +291,8 @@ void *server2(void *arg) {
 		int intervalTotal = p->departureTime - p->arrivalQ1;
 		int a = intervalTotal / 1000;
 		int b = intervalTotal % 1000;
-		printf("p%d departs from S2, service time = %d.%dms, time in system = %d.%dms\n", p->packetId, x, y, a ,b);
+		printf("p%d departs from S2, service time = %d.%dms, time in system = %d.%dms\n",
+				p->packetId, x, y, a ,b);
 		pthread_mutex_unlock(&mutex);
 	}
 	return (void*)0;
@@ -268,11 +321,18 @@ void init() {
 void printParamter() {
 	printf("Emulation Parameters:\n");
 	printf("    number to arrive = %d\n", num);
-	printf("    lambda = %lf\n", lambda);
-	printf("    mu = %lf\n", mu);
+	if(!traceDrivenMode) {
+		printf("    lambda = %lf\n", lambda);
+		printf("    mu = %lf\n", mu);
+	}
 	printf("    r = %lf\n", r);
 	printf("    B = %d\n", B);
-	printf("    P = %d\n", P);
+	if(!traceDrivenMode) {
+		printf("    P = %d\n", P);
+	}
+	if(traceDrivenMode) {
+		printf("tsfile = %s\n", filePath);
+	}
 	printf("\n");
 }
 
@@ -280,8 +340,12 @@ void printParamter() {
 void createThread() {
 	printTime();
 	printf("emulation begins\n");
+	if(!traceDrivenMode) {
+		pthread_create(&pat, NULL, packetArrival, (void*)0);
+	} else {
+		pthread_create(&pat_td, NULL, packetArrivalTraceDriven, (void*)0);
+	}
 	pthread_create(&tdt, NULL, tokenDeposit, (void*)0);
-	pthread_create(&pat, NULL, packetArrival, (void*)0);
 	pthread_create(&s1, NULL, server1, (void*)0);
 	pthread_create(&s2, NULL, server2, (void*)0);
 }
